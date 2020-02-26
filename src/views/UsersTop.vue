82526abb7cc3be708d0411f9e7171de28424431a
Ariel Huang <ariel92911@gmail.com>
2020-02-26 10:13:52 +0800

    add component-UserCard in UsersTop page

--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -2,45 +2,65 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">
-      美食達人
-    </h1>
-    <hr>
+    <h1 class="mt-5">美食達人</h1>
+    <hr />
     <div class="row text-center">
-      <div class="col-3">
-        <a href="#">
-          <img
-            src="http://via.placeholder.com/300x300?text=No+Image"
-            width="140px"
-            height="140px"
-          >
-        </a>
-        <h2>User</h2>
-        <span class="badge badge-secondary">追蹤人數：3</span>
-        <p class="mt-3">
-          <button
-            type="button"
-            class="btn btn-danger"
-          >
-            取消追蹤
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-          >
-            追蹤
-          </button>
-        </p>
-      </div>
+      <!-- User卡片 UserCard-->
+      <UserCard v-for="user in users" :key="user.id" :initial-user="user" />
     </div>
   </div>
 </template>
 
 <script>
 import NavTabs from "./../components/NavTabs";
+import UserCard from "./../components/UserCard";
+
+const dummyData = {
+  users: [
+    {
+      id: 1,
+      name: "Root",
+      email: "root@example.com",
+      image: "https://i.pravatar.cc/300?img=1",
+      isAdmin: true,
+      follower: 3
+    },
+    {
+      id: 2,
+      name: "User1",
+      email: "user1@example.com",
+      image: "https://i.pravatar.cc/300?img=2",
+      isAdmin: false,
+      follower: 0
+    },
+    {
+      id: 3,
+      name: "User2",
+      email: "user2@example.com",
+      image: "https://i.pravatar.cc/300?img=3",
+      isAdmin: false,
+      follower: 2
+    }
+  ]
+};
+
 export default {
   components: {
-    NavTabs
+    NavTabs,
+    UserCard
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      this.users = dummyData.users;
+    }
   }
 };
 </script>
