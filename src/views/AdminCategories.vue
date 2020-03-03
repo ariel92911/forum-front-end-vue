5a4a93e948f8895992e7fce6574387c723c74bd3
Ariel Huang <ariel92911@gmail.com>
2020-03-03 17:30:02 +0800

    add updateCategory function

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -25,11 +25,30 @@
       <tbody>
         <tr v-for="category in categories" :key="category.id">
           <th scope="row">{{ category.id }}</th>
+
           <td class="position-relative">
-            <div class="category-name">{{ category.name }}</div>
+            <div v-show="!category.isEditing" class="category-name">{{ category.name }}</div>
+            <input
+              v-show="category.isEditing"
+              v-model="category.name"
+              type="text"
+              class="form-control"
+            />
+            <span v-show="category.isEditing" class="cancel" @click="handleCancel(category.id)">✕</span>
           </td>
           <td class="d-flex justify-content-between">
-            <button type="button" class="btn btn-link mr-2">Edit</button>
+            <button
+              v-show="!category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click.stop.prevent="toggleIsEditing(category.id)"
+            >Edit</button>
+            <button
+              v-show="category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click.stop.prevent="updateCategory({ categoryId: category.id, name: category.name })"
+            >Save</button>
             <button
               type="button"
               class="btn btn-link mr-2"
@@ -93,7 +112,11 @@ export default {
   methods: {
     // 4. 定義 `fetchCategories` 方法，把 `dummyData` 帶入 Vue 物件
     fetchCategories() {
-      this.categories = dummyData.categories;
+      // 在每一個 category 中都添加一個 isEditing 屬性
+      this.categories = dummyData.categories.map(category => ({
+        ...category,
+        isEditing: false
+      }));
     },
     createCategory(name) {
       console.log("name", name);
@@ -114,7 +137,67 @@ export default {
       this.categories = this.categories.filter(
         category => category.id !== categoryId
       );
+    },
+    toggleIsEditing(categoryId) {
+      this.categories = this.categories.map(category => {
+        if (category.id !== categoryId) return category;
+        // 如果迴圈中的 category.id 是欲修改的 categoryId 則改變 isEditing 的值
+        return {
+          ...category,
+          nameCached: category.name,
+          isEditing: !category.isEditing
+        };
+      });
+    },
+    updateCategory({ categoryId, name }) {
+      console.log("name", name);
+      // TODO: 透過 API 去向伺服器更新餐廳類別名稱
+      this.toggleIsEditing(categoryId);
+    },
+    handleCancel(categoryId) {
+      this.categories = this.categories.map(category => {
+        if (category.id !== categoryId) {
+          return category;
+        }
+
+        // 將原本的類別名稱還回去
+        return {
+          ...category,
+          name: category.nameCached
+        };
+      });
+      this.toggleIsEditing(categoryId);
     }
   }
 };
 </script>
+
+<style scoped>
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
