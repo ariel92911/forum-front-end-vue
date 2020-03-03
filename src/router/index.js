eef4d38982b4376bafe45bf8a0841209b6b16c06
Ariel Huang <ariel92911@gmail.com>
2020-03-03 16:18:04 +0800

    add AdminCategories page and createCategory function

--- src/router/index.js
+++ src/router/index.js
@@ -73,6 +73,11 @@ const routes = [
     component: () => import('../views/AdminRestaurantNew.vue')
   },
   {
+    path: '/admin/categories',
+    name: 'admin-categories',
+    component: () => import('../views/AdminCategories.vue')
+  },
+  {
     path: '/admin/restaurants/:id/edit',
     name: 'admin-restaurant-edit',
     component: () => import('../views/AdminRestaurantEdit.vue')
