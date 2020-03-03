05160d15a6e2d042233c7eb93ad049f602277d05
Ariel Huang <ariel92911@gmail.com>
2020-03-03 14:45:34 +0800

    update AdminRestaurantForm function

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -1,10 +1,11 @@
 // ./src/components/AdminRestaurantForm.vue
 <template>
-  <form>
+  <form @submit.stop.prevent="handleSubmit">
     <div class="form-group">
       <label for="name">Name</label>
       <input
         id="name"
+        v-model="restaurant.name"
         type="text"
         class="form-control"
         name="name"
@@ -15,11 +16,19 @@
 
     <div class="form-group">
       <label for="categoryId">Category</label>
-      <select id="categoryId" class="form-control" name="categoryId" required>
+      <select
+        id="categoryId"
+        v-model="restaurant.categoryId"
+        class="form-control"
+        name="categoryId"
+        required
+      >
         <option value selected disabled>--請選擇--</option>
-        <option value="1">中式料理</option>
-        <option value="2">日式料理</option>
-        <option value="3">義式料理</option>
+        <option
+          v-for="category in categories"
+          :key="category.id"
+          :value="category.id"
+        >{{ category.name }}</option>
       </select>
     </div>
 
@@ -27,6 +36,7 @@
       <label for="tel">Tel</label>
       <input
         id="tel"
+        v-model="restaurant.tel"
         type="text"
         class="form-control"
         name="tel"
@@ -38,6 +48,7 @@
       <label for="address">Address</label>
       <input
         id="address"
+        v-model="restaurant.address"
         type="text"
         class="form-control"
         placeholder="Enter address"
@@ -47,17 +58,43 @@
 
     <div class="form-group">
       <label for="opening-hours">Opening Hours</label>
-      <input id="opening-hours" type="time" class="form-control" name="opening_hours" />
+      <input
+        id="opening-hours"
+        v-model="restaurant.openingHours"
+        type="time"
+        class="form-control"
+        name="opening_hours"
+      />
     </div>
 
     <div class="form-group">
       <label for="description">Description</label>
-      <textarea id="description" class="form-control" rows="3" name="description" />
+      <textarea
+        id="description"
+        v-model="restaurant.description"
+        class="form-control"
+        rows="3"
+        name="description"
+      />
     </div>
 
     <div class="form-group">
       <label for="image">Image</label>
-      <input id="image" type="file" name="image" accept="image/*" class="form-control-file" />
+      <img
+        v-if="restaurant.image"
+        :src="restaurant.image"
+        class="d-block img-thumbnail mb-3"
+        width="200"
+        height="200"
+      />
+      <input
+        id="image"
+        type="file"
+        name="image"
+        accept="image/*"
+        class="form-control-file"
+        @change="handleFileChange"
+      />
     </div>
 
     <button type="submit" class="btn btn-primary">送出</button>
@@ -65,6 +102,35 @@
 </template>
 
 <script>
+const dummyData = {
+  categories: [
+    {
+      id: 1,
+      name: "中式料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    },
+    {
+      id: 2,
+      name: "日本料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    },
+    {
+      id: 3,
+      name: "義大利料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    },
+    {
+      id: 4,
+      name: "墨西哥料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    }
+  ]
+};
+
 export default {
   data() {
     return {
@@ -79,6 +145,26 @@ export default {
       },
       categories: []
     };
+  },
+  created() {
+    this.fetchCategories();
+  },
+  methods: {
+    fetchCategories() {
+      this.categories = dummyData.categories;
+    },
+    handleFileChange(e) {
+      const files = e.target.files;
+      if (!files.length) return; // 如果沒有檔案則離開此函式
+      // 否則產生預覽圖...
+      const imageURL = window.URL.createObjectURL(files[0]);
+      this.restaurant.image = imageURL;
+    },
+    handleSubmit(e) {
+      const form = e.target; // <form></form>
+      const formData = new FormData(form);
+      this.$emit("after-submit", formData);
+    }
   }
 };
 </script>
--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantForm />
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
   </div>
 </template>
 
@@ -12,6 +12,14 @@ import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue";
 export default {
   components: {
     AdminRestaurantForm
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      // 透過 API 將表單資料送到伺服器
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ": " + value);
+      }
+    }
   }
 };
 </script>
