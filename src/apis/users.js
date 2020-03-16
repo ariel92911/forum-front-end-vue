ac9755caf07daed483c55920c2d73e32ced4eafb
Ariel Huang <ariel92911@gmail.com>
2020-03-16 16:27:21 +0800

    install Vuex

--- src/apis/users.js
+++ src/apis/users.js
@@ -3,6 +3,21 @@ import { apiHelper } from './../utils/helpers'
 const getToken = () => localStorage.getItem('token')
 
 export default {
+  getCurrentUser() {
+    return apiHelper.get(`/get_current_user`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  get({ userId }) {
+    return apiHelper.get(`/users/${userId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  getTopUsers() {
+    return apiHelper.get('/users/top', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
   addFavorite({ restaurantId }) {
     return apiHelper.post(`/favorite/${restaurantId}`, null, {
       headers: { Authorization: `Bearer ${getToken()}` }
@@ -20,11 +35,6 @@ export default {
   },
   deleteLike({ restaurantId }) {
     return apiHelper.delete(`/like/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
-  },
-  getTopUsers() {
-    return apiHelper.get('/users/top', {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
   },
