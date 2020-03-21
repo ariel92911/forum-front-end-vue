45ada9eee05841ee818d11efdc085df1a283e17b
Ariel Huang <ariel92911@gmail.com>
2020-03-21 21:36:08 +0800

    Authentication Flow Optimization

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,8 @@ export default new Vuex.Store({
       name: '',
       email: '',
       image: '',
-      isAdmin: false
+      isAdmin: false,
+      token: ''
     },
     isAuthenticated: false
   },
@@ -22,12 +23,16 @@ export default new Vuex.Store({
         // 將 API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
         ...currentUser
       }
+      // 將使用者驗證用的 token 儲存在 state 中
+      state.token = localStorage.getItem('token')
       // 將使用者的登入狀態改為 true
       state.isAuthenticated = true
     },
     revokeAuthentication(state) {
       state.currentUser = {}
       state.isAuthenticated = false
+      // 登出時一並將 state 內的 token 移除
+      state.token = ''
       localStorage.removeItem('token')
     }
   },
@@ -50,6 +55,8 @@ export default new Vuex.Store({
         return true  // add this line
       } catch (error) {
         console.error('can not fetch user information')
+        // 驗證失敗的話一併觸發登出的行為，以清除 state 中的 token
+        commit('revokeAuthentication')
         return false  // add this line
       }
     }
